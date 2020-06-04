e4e507e01d21ed80d33edd3a571a555e495d4ca6
Agustin Pontura <pontura@gmail.com>
2020-06-04 14:34:30 -0300

    clase 1

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float speed = 50;
+
+    public void OnInit(float offsetY)
+    {
+        transform.position += new Vector3(0, offsetY, 0);
+        transform.position += transform.forward * 3;
+        Invoke("Reset", 5);
+        GetComponent<Rigidbody>().AddForce(transform.forward * 2000);
+    }
+
+    //void Update()
+    //{
+    //    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    //}
+    void Reset()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float rotationSpeed = 100;
+    public float moveSpeed = 10;
+    public float jumpForce = 100;
+    public Animator anim;
+    public Bullet bullet;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            Rotate(-1);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            Rotate(1);
+        if(Input.GetKey(KeyCode.UpArrow))
+            MoveForward(1);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            MoveForward(-1);
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+        if (Input.GetKeyDown(KeyCode.Z))
+            Shoot();
+    }
+    void Rotate(int direction)
+    {
+        transform.Rotate(Vector3.up * direction * (rotationSpeed * Time.deltaTime));
+    }
+    void MoveForward(int direction)
+    {
+        anim.Play("walk");
+        transform.Translate(Vector3.forward * direction * (moveSpeed * Time.deltaTime));
+    }
+    void Jump()
+    {
+        GetComponent<Rigidbody>().AddForce( Vector3.up * jumpForce );
+    }
+    void Shoot()
+    {
+        Bullet newBullet = Instantiate(bullet);
+        newBullet.transform.position = transform.position;
+        newBullet.transform.rotation = transform.rotation;
+        newBullet.OnInit(3);
+    }
+}
